67302403b5238166a523b808830e01d30fcd0d7f
Pavan Mistry <pavan32100@gmail.com>
2024-10-13 23:19:34 -0400

    Added panel Dashboard

--- functions/panels.php
+++ functions/panels.php
@@ -1,7 +1,8 @@
 <?php
 
 // Fetch panel data based on city_id
-function get_panel_data_by_city($city_id, $connect) {
+function get_panel_data_by_city($city_id, $connect)
+{
     $query = "SELECT * FROM panels WHERE city_id = " . mysqli_real_escape_string($connect, $city_id);
     $result = mysqli_query($connect, $query);
 
@@ -14,13 +15,16 @@ function get_panel_data_by_city($city_id, $connect) {
 }
 
 // Update panel value based on panel ID
-function update_panel_value($panel_id, $new_value, $connect) {
+function update_panel_value($panel_id, $new_value, $connect)
+{
     $query = "UPDATE panels SET value = '" . mysqli_real_escape_string($connect, $new_value) . "' WHERE id = '" . mysqli_real_escape_string($connect, $panel_id) . "'";
-    mysqli_query($connect, $query);
+    $result = mysqli_query($connect, $query);
+    return $result;
 }
 
 // Group panel data by cartridge and port_id
-function group_panel_data_by_cartridge($panel_data) {
+function group_panel_data_by_cartridge($panel_data)
+{
     $power_lever = [];
     $current_cartridge = [];
     $cartridge_data = [];
@@ -50,4 +54,23 @@ function group_panel_data_by_cartridge($panel_data) {
     }
 
     return [$power_lever, $current_cartridge, $cartridge_data];
+}
+
+// Fetch panel data based on cartridge and city_id
+function get_panel_data_by_cartridge($cartridge, $city_id, $connect)
+{
+    $query = "SELECT * FROM panels WHERE city_id = " . mysqli_real_escape_string($connect, $city_id) . " 
+              AND cartridge = '" . mysqli_real_escape_string($connect, $cartridge) . "'";
+    $result = mysqli_query($connect, $query);
+
+    $cartridge_data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $port_id = $row['port_id'];
+        if (!isset($cartridge_data[$port_id])) {
+            $cartridge_data[$port_id] = [];
+        }
+        $cartridge_data[$port_id][] = ['id' => $row['id'], 'value' => $row['value']];
+    }
+
+    return $cartridge_data;
 }
